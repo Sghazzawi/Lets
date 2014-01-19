365ba66edbf2fce95b42c509c1047eb7ede05d25
Sghazzawi <Sharif.Ghazzawi@gmail.com>
2014-01-19 18:36:04 +0000

    reverted change

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.logger());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
@@ -23,7 +24,6 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.use(express.logger());
 });
 
 app.configure('production', function(){
